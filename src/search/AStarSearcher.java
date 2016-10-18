2566a07063f63e5dfb6fddf31469e3bd9417bdf7
Lulu1494 <Lulu1494@gmail.com>
2016-10-18 10:14:31 -0700

    First attempt at AStarSearcher
    It extends BreadthFirstSearcher, using a PriorityQueue for
    `frontier`instead of a LinkedList

--- src/search/AStarSearcher.java
+++ src/search/AStarSearcher.java
@@ -1,38 +1,13 @@
 package search;
 
-import java.util.LinkedList;
 import java.util.Map;
+import java.util.PriorityQueue;
 
-public class AStarSearcher implements Searcher {
-
-	@Override
-	public void addToFrontier(Node node) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean cutoff(Node node) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean frontierIsEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public int getNodeCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+public class AStarSearcher extends BreadthFirstSearcher {
 
 	@Override
 	public void initializeFrontier() {
-		// TODO Auto-generated method stub
-
+		frontier = new PriorityQueue<Node>();
 	}
 
 	@Override
@@ -40,23 +15,4 @@ public class AStarSearcher implements Searcher {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-	@Override
-	public Node removeFromFrontier() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void resetNodeCount() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public LinkedList<Character> search(Node problem) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
 }
--- src/search/BreadthFirstSearcher.java
+++ src/search/BreadthFirstSearcher.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 public class BreadthFirstSearcher implements Searcher {
 
-	private Queue<Node> frontier;
+	protected Queue<Node> frontier;
 	private int nodeCount;
 	private int cutoffDepth;
 	
